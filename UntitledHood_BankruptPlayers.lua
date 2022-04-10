db151edbdb23a205246d642df75aec60a0208c25
Kitzoon <68196838+Kitzoon@users.noreply.github.com>
2022-04-10 01:19:26 -0600

    Update UntitledHood_BankruptPlayers.lua

--- UntitledHood_BankruptPlayers.lua
+++ UntitledHood_BankruptPlayers.lua
@@ -1,4 +1,4 @@
---local player = "target of name here" uncomment out this line if you arent running this from the loadstring
+--local player = "target name here" uncomment out this line if you arent running this from the loadstring
 
 game.ReplicatedStorage:FindFirstChild(".gg/untitledhood"):FireServer(
    "Reload",
