99cdc7b53d367df566f2c7517c7b668d8edcb4c4
Kitzoon <68196838+Kitzoon@users.noreply.github.com>
2022-09-28 13:36:05 -0400

    Create WalmartLand.lua

---
+++ WalmartLand.lua
@@ -0,0 +1,17 @@
+if game.PlaceId ~= 10895555747 then return end
+
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local localPlr = game:GetService("Players").LocalPlayer
+
+for _,v in pairs(workspace["DAILY_TOKENS"]:GetChildren()) do
+    firetouchinterest(localPlr.Character.HumanoidRootPart, v, 0)
+    firetouchinterest(localPlr.Character.HumanoidRootPart, v, 1)
+end
+
+for _,v in pairs(workspace["UNIQUE_TOKENS"]:GetChildren()) do
+    firetouchinterest(localPlr.Character.HumanoidRootPart, v, 0)
+    firetouchinterest(localPlr.Character.HumanoidRootPart, v, 1)
+end
