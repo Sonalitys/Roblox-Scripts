2497bc79697e16638f3f49748edd973750af804c
Kitzoon <68196838+Kitzoon@users.noreply.github.com>
2022-10-15 16:15:57 -0400

    Update FIFAWorldCollectLetters.lua

--- FIFAWorldCollectLetters.lua
+++ FIFAWorldCollectLetters.lua
@@ -2,17 +2,5 @@
 -- // SKIDS NEVER WIN, ONLY USE THIS SOURCE CODE TO LEARN B) \\
 -- // https://www.roblox.com/games/9486506804 \\
 
-if not game:IsLoaded() then
-    game.Loaded:Wait()
-end
-
-if game.PlaceId ~= 9486506804 then return end
-
-local localPlr = game:GetService("Players").LocalPlayer
-
-for _, letter in pairs(workspace.ScavengerHunt.Step1.Objects:GetChildren()) do
-    if string.find(letter.Name, "Symbol") and localPlr.Character ~= nil then
-        firetouchinterest(localPlr.Character.HumanoidRootPart, letter.Root, 0)
-        firetouchinterest(localPlr.Character.HumanoidRootPart, letter.Root, 1)
-    end
-end
+-- ive updated the script to a gui lol
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kitzoon/Rogue-Hub/main/Main.lua", true))()
